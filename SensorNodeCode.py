b81940c97b09d5caff64af772f2b312dcd0c7f62
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2022-12-19 13:58:33 +0100

    Continuation...

--- SensorNodeCode.py
+++ SensorNodeCode.py
@@ -2,6 +2,8 @@ from machine import Pin
 from machine import Timer
 from machine import ADC
 from machine import UART
+import time
+import str
 
 moisure = ADC(31)
 temperature = ADC(32)
@@ -11,22 +13,24 @@ timer = Timer()
 moisureMeas=0
 temperatureMeas=0
 head=0
-nodeID=0
+nodeID=hex(0).lstrip("0x")
 battery=0
 messageHead=f'AT+MSGHEX="{hex(head).lstrip("0x")}{hex(nodeID).lstrip("0x")}'
 
-def makeCRC(code):
-    binarycode=bin(code)
+# def makeCRC(code):
+#     binarycode=bin(code)
     
 
 def measureNsend(timer):
     global moisure
     global temperature
     moisureMeas=hex(moisure.read()).lstrip("0x")
+    time.sleep(1)
     temperatureMeas=hex(temperature.read()).lstrip("0x")
-    messageMeas=f'{hex(battery).lstrip("0x")}{moisureMeas}{temperatureMeas}'
-    CNC=
-    uart.write()
+    time.sleep(1)
+    uart.write(f'AT+MSGHEX="{nodeID}{temperatureMeas}{moisureMeas}"')
 
 #def sendlora (timer):
     #uart.write(buf)
+
+timer.init(freq=0.0005, mode=Timer.PERIODIC, callback=measureNsend)

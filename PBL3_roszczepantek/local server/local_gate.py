39208549a86b795313397090ca10a48a2a4328de
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2022-12-20 20:20:07 +0100

    Update local_gate.py

--- PBL3_roszczepantek/local server/local_gate.py
+++ PBL3_roszczepantek/local server/local_gate.py
@@ -41,7 +41,7 @@ def loraConf(id, port):
     sleep(0.5)
     print(f'Reseting LoRa module to default: {last_response}')
     sendAT('AT+MODE=TEST')
-    sleep(0.5)
+    sleep(1)
     last_response = sendAT('AT+MODE')
     print(f'Changing LoRa module mode to TEST: {last_response}')
     return 1

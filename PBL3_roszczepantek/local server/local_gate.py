9ef43ecdda9e4b3b890de3fb48947be3bf6208f1
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2022-12-20 23:24:50 +0100

    Update local_gate.py

--- PBL3_roszczepantek/local server/local_gate.py
+++ PBL3_roszczepantek/local server/local_gate.py
@@ -49,20 +49,20 @@ def loraConf(id, port):
     return 1
 
 def dataProcess (msg):
-    s_nodeID = [f'{msg[0]}{msg[1]}', f'{msg[2]}{msg[3]}']
-    s_temperature_meas = [f'{msg[4]}{msg[5]}'] 
-    s_moisture_meas = [f'{msg[6]}{msg[7]}']
+    s_nodeID = [f'0x{msg[0]}{msg[1]}{msg[2]}{msg[3]}']
+    s_temperature_meas = f'0x{msg[4]}{msg[5]}'
+    s_moisture_meas = f'0x{msg[6]}{msg[7]}'
     print(s_nodeID, s_temperature_meas, s_moisture_meas)
-    h_nodeID = [eval(x) for x in s_nodeID]
-    h_temperature_meas = [eval(x) for x in s_temperature_meas]
-    h_moisture_meas = [eval(x) for x in s_moisture_meas]
+    h_nodeID = [int(s_nodeID[0], 16), int(s_nodeID[1], 16)]
+    h_temperature_meas = int(s_temperature_meas,16)
+    h_moisture_meas = int(s_moisture_meas,16)
     print(h_nodeID, h_temperature_meas, h_moisture_meas)
-    nodeID = [int(x,base=16) for x in h_nodeID]
-    temperature_meas = [int(x,base=16) for x in h_temperature_meas]
-    moisture_meas = [int(x,base=16) for x in h_moisture_meas]
+    #nodeID = [int(x,base=16)]
+    #temperature_meas = [int(x,base=16) for x in h_temperature_meas]
+    #moisture_meas = [int(x,base=16) for x in h_moisture_meas]
     
-    processed_data = [nodeID, temperature_meas, moisture_meas]
-    return processed_data
+    #processed_data = [nodeID, temperature_meas, moisture_meas]
+    #return processed_data
     
         
     

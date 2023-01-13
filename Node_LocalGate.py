e56461117181c2668aa736d2cedc4d6eeeaded99
Szczpan <01168997@pw.edu.pl>
2023-01-13 17:29:55 +0100

    decrease valve open time

--- Node_LocalGate.py
+++ Node_LocalGate.py
@@ -189,6 +189,7 @@ if __name__ == "__main__":
 
             for valve in valve_list:
                 time_left -= time() - last_time
+                print(str(time_left) + "\n")
                 if time_left > 0:
                     valve_obj = ValveNode(valve, True, time_left)
                     update_valve(MY_ID, valve_obj)

77f764b8f72b04e7526d766afa0e1b792cf73ef6
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2023-01-13 16:57:22 +0100

    Update Node_LocalGate.py

--- Node_LocalGate.py
+++ Node_LocalGate.py
@@ -50,7 +50,7 @@ def receiveData():
 #CONFIG FUNCTION FOR MODULE
 def loraConf():
     readData()
-    if connectTest() == '+AT: OK\r\n':
+    if not '+AT: OK\r\n' in connectTest():
         return 0
     last_response = sendAT('AT+RESET')
     sleep(0.5)

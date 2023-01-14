b9c929970fe9b7672f8b328e7fbc5dbf0151ee09
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2023-01-14 23:11:30 +0100

    Zarządzanie podlewaniem (usprawniona komunikacja)

--- Node_LocalGate.py
+++ Node_LocalGate.py
@@ -27,51 +27,59 @@ if __name__ == "__main__":
             
             nodes = getLora(UNIVERSAL_MODE, sensor_id_list, valve_id_list)
 
-            if nodes != None:
-                if nodes.SensorNode != None:
-                    (nodes.SensorNode).print_data()
-
-                    if nodes.SensorNode.sensor_id in sensor_id_list:
-                        update_sensor(MAIN_ID, nodes.SensorNode)
-                
-                if nodes.ValveNode != None:
-                    valve = nodes.ValveNode
-                    valve.valve_id = VALVE_ID
-                    valve.print_data()
-            
             forecast_rain = get_rain_sum()
             soil_avg = get_sensor_soil(MAIN_ID)
-
+            
             if nodes != None:
                 if nodes.SensorNode != None:
                     sensor = nodes.SensorNode
-                    if soil_avg*forecast_rain < 200:
-                        for valve in valve_id_list:
-                            valve_obj = ValveNode(valve, True, 100)
-                            time_left = 100
-                            update_valve(MAIN_ID, valve_obj)
-                    else:
-                        for valve in valve_id_list:
-                            valve_obj = ValveNode(valve, False, 0)
-                            time_left = 0
-                            update_valve(MAIN_ID, valve_obj)
-                    last_time = time()
-
-            for valve in valve_id_list:
-                time_left -= time() - last_time
-                if time_left > 0:
-                    valve_obj = ValveNode(valve, True, time_left)
-                    update_valve(MAIN_ID, valve_obj)
-                    send_data_hex(valve_obj.hex_str())
-                else:
-                    valve_obj = ValveNode(valve, False, 0)
-                    time_left = 0
-                    update_valve(MAIN_ID, valve_obj)
-                    send_data_hex(valve_obj.hex_str())
-                last_time = time()
-
+                    sensor.print_data()
+                    if sensor.sensor_id in sensor_id_list:
+                        update_sensor(MAIN_ID, sensor)
+                        
+                if nodes.ValveNode != None:
+                    valve = nodes.ValveNode
+                    valve.print_data()
+                    update_valve(MAIN_ID, valve)
+            
+            if soil_avg*forecast_rain < 200:
+                for valve_id in valve_id_list:
+                    time_left = 100
+                    valve_tmp = ValveNode(valve_id, True, time_left)
+                    send_data_hex(valve_tmp.hex_str())
+                    sleep(0.5)
+                    
             sleep(0.5)
             
         except KeyboardInterrupt:
             print('\nProgram executed with keyboard interrupt')
             exit()
+            
+
+            # if nodes != None:
+            #     if nodes.SensorNode != None:
+            #         sensor = nodes.SensorNode
+            #         if soil_avg*forecast_rain < 200:
+            #             for valve in valve_id_list:
+            #                 valve_obj = ValveNode(valve, True, 100)
+            #                 time_left = 100
+            #                 update_valve(MAIN_ID, valve_obj)
+            #         else:
+            #             for valve in valve_id_list:
+            #                 valve_obj = ValveNode(valve, False, 0)
+            #                 time_left = 0
+            #                 update_valve(MAIN_ID, valve_obj)
+            #         last_time = time()
+
+            # for valve in valve_id_list:
+            #     time_left -= time() - last_time
+            #     if time_left > 0:
+            #         valve_obj = ValveNode(valve, , time_left)
+            #         update_valve(MAIN_ID, valve_obj)
+            #         send_data_hex(valve_obj.hex_str())
+            #     else:
+            #         valve_obj = ValveNode(valve, False, 0)
+            #         time_left = 0
+            #         update_valve(MAIN_ID, valve_obj)
+            #         send_data_hex(valve_obj.hex_str())
+            #     last_time = time()
--- Node_Valve.py
+++ Node_Valve.py
@@ -1,9 +1,12 @@
 from Node_functions import loraConf, send_data_hex, getLora, VALVE_ID, VALVE_MODE, VALVE_PIN
 from operations import ValveNode
-from time import sleep
+from time import time, sleep
 import RPi.GPIO as GPIO
 
-valve = ValveNode(VALVE_ID, False, 0)
+MY_ID = VALVE_ID
+valve = ValveNode(MY_ID, False, 0)
+time_left = 0
+last_time = time()
 
 if __name__ == "__main__":
     if loraConf() == 0:
@@ -12,23 +15,33 @@ if __name__ == "__main__":
         
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(VALVE_PIN, GPIO.OUT)
-    
+
     while True:
         try:
-            nodes = getLora(VALVE_MODE, [], [VALVE_ID])
+            nodes = getLora(VALVE_MODE, [], [MY_ID])
             
             if nodes != None:
                 if nodes.ValveNode != None:
                     valve = nodes.ValveNode
-                    valve.valve_id = VALVE_ID
                     valve.print_data()
-                    if valve.time_left > 0:
-                        GPIO.output(21, GPIO.HIGH)
-                        valve.is_open = True
-                    elif valve.time_left == 0:
-                        GPIO.output(21, GPIO.LOW)
-                        valve.is_open = False
-                        
+                    if valve.valve_id == MY_ID:
+                        if valve.time_left > 0:
+                            GPIO.output(21, GPIO.HIGH)
+                            valve.is_open = True
+                            time_left = valve.time_left
+                        elif valve.time_left == 0:
+                            GPIO.output(21, GPIO.LOW)
+                            valve.is_open = False
+                            time_left = 0
+            
+            if time_left != 0:
+                time_left -= time() - last_time
+                last_time = time()
+                if time_left <= 0: 
+                    time_left = 0
+                    GPIO.output(21, GPIO.LOW)
+                    valve.is_open = False
+            
             send_data_hex(valve.hex_str())
             print(valve.hex_str())            
             sleep(0.5)

111030bf9cbb5725e72680f8a27a119e6d757277
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2023-01-22 16:09:40 +0100

    Update Node_functions.py

--- Node_functions.py
+++ Node_functions.py
@@ -154,6 +154,7 @@ def getLora(mode, list_of_sensor_nodes, list_of_valve_nodes):
     # print(f'Wybrany tryb to: {mode}')
 
     if RAW_msg != ' ' and RAW_msg != '':
+        print(f'Długość odebranej wiadomosci: {len(RAW_msg)}')
         node_id = checkNodeID(RAW_msg)
         # print(f"Sprawdzanie czy node {node_id} jest na liscie {list_of_nodes}")
         if node_id in list_of_nodes:

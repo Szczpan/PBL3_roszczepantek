9c3df3881a02bb55a694e354c2f2af3e8cba826d
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2023-01-22 18:02:05 +0100

    Update Node_functions.py

--- Node_functions.py
+++ Node_functions.py
@@ -198,6 +198,7 @@ def checkNodeID(RAW_msg):
         msg_index = tmp_msg.find('"', msg_index, len(tmp_msg)+1) + 1
         tmp_msg = tmp_msg[msg_index:]
         if tmp_msg[0] == 0:
+            print('zapisuje node')
             node_id = int(f'0x{tmp_msg[0]}{tmp_msg[1]}{tmp_msg[2]}{tmp_msg[3]}',16)
             node_list.append(node_id)
     return node_list

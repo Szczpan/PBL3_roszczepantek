75d4d68ea480a0130f71c9bb4c57452706989a50
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-01-24 23:23:01 +0100

    GPIO correction

--- czujniki.py
+++ czujniki.py
@@ -7,19 +7,22 @@ import datetime
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
 
 # read data using pin 37 (GPIO26)
-instance = dht11.DHT11(pin=16)
+instance = dht11.DHT11(pin=26)
 
 try:
 	while True:
 		result = instance.read()
-		moisure=GPIO.input(26)
+		moisure=GPIO.input(16)
+		someshit=GPIO.input(26)
 		#if result.is_valid():
 		print("Last valid input: " + str(datetime.datetime.now()))
 		print("Temperature: %-3.1f C" % result.temperature)
 		print("Humidity: %-3.1f %%" % result.humidity)
 		print("Moisure: %-3.1f %%" % moisure)
+		print("someshit: %-3.1f %%" % someshit)
 		time.sleep(6)
 except KeyboardInterrupt:
 	print("Cleanup")

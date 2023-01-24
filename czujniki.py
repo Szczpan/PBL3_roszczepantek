e57b2f92e36f9f92989bd43f60b2f93e48e37940
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-01-24 23:06:15 +0100

    Moisure sensor added

--- czujniki.py
+++ czujniki.py
@@ -6,6 +6,7 @@ import datetime
 # initialize GPIO
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(36, GPIO.IN)
 
 # read data using pin 37 (GPIO26)
 instance = dht11.DHT11(pin=37)
@@ -13,10 +14,12 @@ instance = dht11.DHT11(pin=37)
 try:
 	while True:
 		result = instance.read()
+		moisure=GPIO.input(36)
 		if result.is_valid():
 			print("Last valid input: " + str(datetime.datetime.now()))
 			print("Temperature: %-3.1f C" % result.temperature)
 			print("Humidity: %-3.1f %%" % result.humidity)
+		print("Moisure: %-3.1f %%" % moisure)
 		time.sleep(6)
 except KeyboardInterrupt:
 	print("Cleanup")

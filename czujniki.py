be16c70ac14ee5b27ef454857f09547963ca9aa5
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-01-25 15:57:42 +0100

    chyba dziala kurwaaaaaaa

--- czujniki.py
+++ czujniki.py
@@ -23,16 +23,16 @@ instance = dht11.DHT11(pin=26)
 try:
 	while True:
 		result = instance.read()
-		wateringMin=min(250, 250+100*(result.temperature-15)/6)
+		wateringMin=min(25, 25+10*(result.temperature-15)/6)
 		#os.system("cat /sys/bus/iio/devices/iio\:device0/in_voltage0-voltage1_raw")
 		process = subprocess.Popen(shlex.split("cat /sys/bus/iio/devices/iio\:device0/in_voltage0-voltage1_raw"), stdout=subprocess.PIPE)
 		moistureRaw=process.stdout.readline()
-		print(moistureRaw)
+		#print(moistureRaw)
 		# moistureRaw = re.findall("b'(\d*)\\n'", str(moistureRaw, "utf-8"))
 		# moistureRaw=moistureRaw[2:-2]
 		#print(moistureRaw)
 		moisture=float(moistureRaw)/17670*100
-		print(moisture)
+		#print(moisture)
 		if result.is_valid():
 			print("Last valid input: " + str(datetime.datetime.now()))
 			print("Temperature: %-3.1f C" % result.temperature)

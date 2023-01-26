ead460aecf549b7fc78355c04190b5944e9726d6
Antoni Popecki <antoni.popecki.stud@pw.edu.pl>
2023-01-26 15:56:05 +0100

    Update Node_functions.py

--- Node_functions.py
+++ Node_functions.py
@@ -109,7 +109,7 @@ def getSensorData_random():
 # Gets data from sensors
 def getSensorData(dht_11_pin, moist_sensor_pin):
  	# read data from DHT 11 sensor
-	dht_11_meas = read_DHT_11(dht_11_meas)
+	dht_11_meas = read_DHT_11(dht_11_pin)
 
 	# read data from DFRobot Moisture Sensor v2
 	moist_meas = read_moisture(moist_sensor_pin)

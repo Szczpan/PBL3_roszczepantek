649abb372af81d52590b35e19447bf2345effae9
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-01-26 12:00:29 +0100

    zmienna temp

--- czujniki.py
+++ czujniki.py
@@ -19,34 +19,36 @@ def meas():
 	GPIO.setup(23, GPIO.OUT)
 
 	wateringMin=10
+	temp=1
 
 	# read data using pin 37 (GPIO26)
 	instance = dht11.DHT11(pin=26)
 
 	try:
-		#while True:
-		result = instance.read()
-		wateringMin=min(25, 25+10*(result.temperature-15)/6)
-		#os.system("cat /sys/bus/iio/devices/iio\:device0/in_voltage0-voltage1_raw")
-		process = subprocess.Popen(shlex.split("cat /sys/bus/iio/devices/iio\:device0/in_voltage0-voltage1_raw"), stdout=subprocess.PIPE)
-		moistureRaw=process.stdout.readline()
-		#print(moistureRaw)
-		# moistureRaw = re.findall("b'(\d*)\\n'", str(moistureRaw, "utf-8"))
-		# moistureRaw=moistureRaw[2:-2]
-		# print(moistureRaw)
-		moisture=float(moistureRaw)/17670*100
-		#print(moisture)
-		if result.is_valid():
-			print("Last valid input: " + str(datetime.datetime.now()))
-			print("Temperature: %-3.1f C" % result.temperature)
-			print("Humidity: %-3.1f %%" % result.humidity)
-			print("Moisture: %-3.1f %%" % moisture)
-			time.sleep(6)
-			if moisture < wateringMin:
-				GPIO.output(23,1)
-				print("podlewam")
-				time.sleep(4)
-				GPIO.output(23,0)
+		while temp:
+			result = instance.read()
+			wateringMin=min(25, 25+10*(result.temperature-15)/6)
+			#os.system("cat /sys/bus/iio/devices/iio\:device0/in_voltage0-voltage1_raw")
+			process = subprocess.Popen(shlex.split("cat /sys/bus/iio/devices/iio\:device0/in_voltage0-voltage1_raw"), stdout=subprocess.PIPE)
+			moistureRaw=process.stdout.readline()
+			#print(moistureRaw)
+			# moistureRaw = re.findall("b'(\d*)\\n'", str(moistureRaw, "utf-8"))
+			# moistureRaw=moistureRaw[2:-2]
+			# print(moistureRaw)
+			moisture=float(moistureRaw)/17670*100
+			#print(moisture)
+			if result.is_valid():
+				temp=0
+				print("Last valid input: " + str(datetime.datetime.now()))
+				print("Temperature: %-3.1f C" % result.temperature)
+				print("Humidity: %-3.1f %%" % result.humidity)
+				print("Moisture: %-3.1f %%" % moisture)
+				time.sleep(6)
+				if moisture < wateringMin:
+					GPIO.output(23,1)
+					print("podlewam")
+					time.sleep(4)
+					GPIO.output(23,0)
 
 
 	except KeyboardInterrupt:
